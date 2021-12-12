087dd56d75b43493fc3837e6d03403c0fa8e6b8c
SamsUniAcct <sw16aae@herts.ac.uk>
2021-12-12 00:29:54 -0500

    Added basic UI element structure

--- App.js
+++ App.js
@@ -3,19 +3,82 @@
  * https://github.com/SamsUniAcct/MorseCodeApp
  */
 
-import React from 'react';
-import {
-  View,
-  Text
-} from 'react-native';
-
-const App = () => {
-
-  return (
-    <View>
-      <Text>Test</Text>
-    </View>
-  );
-};
-
-export default App;
+ import React from 'react';
+ import {
+   StyleSheet,
+   Text,
+   TouchableHighlight,
+   View
+ } from 'react-native';
+ 
+ const App = () => {
+   const menuButtonPress = () => {
+     alert('You tapped the button!');
+   };
+   return (
+     <View style={styles.mainContainer}>
+       <View style={styles.header}>
+       </View>
+       <View style={styles.mainSection}>
+       </View>
+       <View style={styles.footer}>
+         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
+           <View >
+             <Text style={styles.footerButtonText}>Touchable</Text>
+           </View>
+         </TouchableHighlight>
+         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
+           <View >
+             <Text style={styles.footerButtonText}>Touchable2</Text>
+           </View>
+         </TouchableHighlight>
+         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
+           <View >
+             <Text style={styles.footerButtonText}>Touchable2</Text>
+           </View>
+         </TouchableHighlight>
+         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
+           <View >
+             <Text style={styles.footerButtonText}>Touchable2</Text>
+           </View>
+         </TouchableHighlight>
+       </View>
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   mainContainer: {
+     flex: 1
+   },
+   header: {
+     flex: 1,
+     backgroundColor: 'powderblue'
+   },
+   mainSection: {
+     flex: 4,
+     backgroundColor: 'skyblue'
+   },
+   footer: {
+     flex: 1,
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     backgroundColor: 'steelblue'
+   },
+   footerButton: {
+     height: '100%',
+     justifyContent: 'space-between',
+     marginLeft: 5,
+     marginRight: 5,
+     flexGrow: 1,
+     flexShrink: 1,
+     flexBasis: "auto",
+     backgroundColor: 'red'
+   },
+   footerButtonText: {
+     color: 'white'
+   }
+ });
+ 
+ export default App;
+ 
