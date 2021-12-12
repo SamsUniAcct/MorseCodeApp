8f29f935f8d078b1d9c1f4ee817aa21931d42bdf
SamsUniAcct <sw16aae@herts.ac.uk>
2021-12-12 03:42:49 -0500

    Navigation and screens implemented

--- App.js
+++ App.js
@@ -3,82 +3,27 @@
  * https://github.com/SamsUniAcct/MorseCodeApp
  */
 
- import React from 'react';
- import {
-   StyleSheet,
-   Text,
-   TouchableHighlight,
-   View
- } from 'react-native';
- 
- const App = () => {
-   const menuButtonPress = () => {
-     alert('You tapped the button!');
-   };
-   return (
-     <View style={styles.mainContainer}>
-       <View style={styles.header}>
-       </View>
-       <View style={styles.mainSection}>
-       </View>
-       <View style={styles.footer}>
-         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
-           <View >
-             <Text style={styles.footerButtonText}>Touchable</Text>
-           </View>
-         </TouchableHighlight>
-         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
-           <View >
-             <Text style={styles.footerButtonText}>Touchable2</Text>
-           </View>
-         </TouchableHighlight>
-         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
-           <View >
-             <Text style={styles.footerButtonText}>Touchable2</Text>
-           </View>
-         </TouchableHighlight>
-         <TouchableHighlight onPress={menuButtonPress} underlayColor="white" style={styles.footerButton}>
-           <View >
-             <Text style={styles.footerButtonText}>Touchable2</Text>
-           </View>
-         </TouchableHighlight>
-       </View>
-     </View>
-   );
- };
- 
- const styles = StyleSheet.create({
-   mainContainer: {
-     flex: 1
-   },
-   header: {
-     flex: 1,
-     backgroundColor: 'powderblue'
-   },
-   mainSection: {
-     flex: 4,
-     backgroundColor: 'skyblue'
-   },
-   footer: {
-     flex: 1,
-     flexDirection: 'row',
-     justifyContent: 'space-between',
-     backgroundColor: 'steelblue'
-   },
-   footerButton: {
-     height: '100%',
-     justifyContent: 'space-between',
-     marginLeft: 5,
-     marginRight: 5,
-     flexGrow: 1,
-     flexShrink: 1,
-     flexBasis: "auto",
-     backgroundColor: 'red'
-   },
-   footerButtonText: {
-     color: 'white'
-   }
- });
- 
- export default App;
- 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Components/Screens/HomeScreen';
+import PhrasesScreen from './Components/Screens/PhrasesScreen';
+import DictionaryScreen from './Components/Screens/DictionaryScreen';
+import SettingsScreen from './Components/Screens/SettingsScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Phrases" component={PhrasesScreen} />
+        <Stack.Screen name="Dictionary" component={DictionaryScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Components/Screens/SettingsScreen.js
@@ -0,0 +1,86 @@
+/**
+ * Morse Code React Native App
+ * https://github.com/SamsUniAcct/MorseCodeApp
+ * Home Screen
+ */
+
+ import React from 'react';
+ import {
+     StyleSheet,
+     Text,
+     TouchableHighlight,
+     View
+ } from 'react-native';
+ 
+ const SettingsScreen = ({ navigation }) => {
+ 
+     return (
+         <View style={styles.mainContainer}>
+             <View style={styles.header}>
+             </View>
+             <View style={styles.mainSection}>
+             </View>
+             <View style={styles.footer}>
+                 <TouchableHighlight onPress={() => navigation.navigate('Home')} underlayColor="white" style={styles.footerButton}>
+                     <View >
+                         <Text style={styles.footerButtonText}>SOS</Text>
+                     </View>
+                 </TouchableHighlight>
+                 <TouchableHighlight onPress={() => navigation.navigate('Phrases')} underlayColor="white" style={styles.footerButton}>
+                     <View >
+                         <Text style={styles.footerButtonText}>Phrases</Text>
+                     </View>
+                 </TouchableHighlight>
+                 <TouchableHighlight onPress={() => navigation.navigate('Dictionary')} underlayColor="white" style={styles.footerButton}>
+                     <View >
+                         <Text style={styles.footerButtonText}>Dictionary</Text>
+                     </View>
+                 </TouchableHighlight>
+                 <TouchableHighlight onPress={() => navigation.navigate('Settings')} underlayColor="white" style={styles.footerButton}>
+                     <View >
+                         <Text style={styles.footerButtonText}>Settings</Text>
+                     </View>
+                 </TouchableHighlight>
+             </View>
+         </View>
+     );
+ 
+ };
+ 
+ const styles = StyleSheet.create({
+     mainContainer: {
+         flex: 1
+     },
+     header: {
+         flex: 1,
+         backgroundColor: 'powderblue'
+     },
+     mainSection: {
+         flex: 4,
+         backgroundColor: 'skyblue'
+     },
+     footer: {
+         flex: 1,
+         flexDirection: 'row',
+         justifyContent: 'space-between',
+         paddingTop: 4,
+         backgroundColor: 'steelblue'
+     },
+     footerButton: {
+         height: '100%',
+         justifyContent: 'center',
+         alignItems: 'center',
+         marginLeft: 2,
+         marginRight: 2,
+         flexGrow: 1,
+         flexShrink: 1,
+         flexBasis: 2,
+         backgroundColor: 'red'
+     },
+     footerButtonText: {
+         color: 'white',
+         alignContent: 'center'
+     }
+ });
+ 
+ export default SettingsScreen;
